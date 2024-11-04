a3a1153ea98dcdf75ce545dd2eee33066b02b068
Sainul Abiheen P <sainulabidheenpsainu@gmail.com>
2024-11-04 20:01:08 +0530

    Add files via upload

---
+++ example1.dart
@@ -0,0 +1,9 @@
+void main(){
+Map<String,dynamic>name={
+'fruits':['apple','orenge','gua'],
+'cars':['bmw','maruti','audi'],
+'languages':["english","hindhi","malayalam"],
+'numbers':["1","2","3","4","5"],
+};
+print(name['numbers']);
+}
